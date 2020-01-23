ccac6ca17f8ca8454d853e4171bd622d284efc0d
zystylish <zystylish@gmail.com>
2020-01-23 17:24:42 +0800

    fix: ffff

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -6,7 +6,6 @@ const io3333 = "ffff";
 const io3333 = "ffff";
 const io3333 = "ffff";
 const io3333 = "ffff";
-const io3333 = "ffff";
 
 export default class App extends React.Component {
   render() {
