061db0367d0146c0546afcb15ba14ffba9915a83
zystylish <zystylish@gmail.com>
2020-01-23 17:19:12 +0800

    feat: fix

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const io = "ffff";
+const io = "ffff";
+const io3333 = "ffff";
+const io3333 = "ffff";
+const io3333 = "ffff";
+const io3333 = "ffff";
+const io3333 = "ffff";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>hello world22223333111</p>
+      </div>
+    );
+  }
+}
